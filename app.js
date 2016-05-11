63f53bfb45b886d0119b527b729716d43e415d59
Andrew Clark <AndrewClark@Andrews-MacBook-Pro.local>
2016-05-11 11:46:34 -0400

    attaching images of cards to 52 different classes

--- app.js
+++ app.js
@@ -20,9 +20,8 @@ var datValue = function(){
 
                     if (total > 21){ 
                      alert("BUSTEDDDDDDDDD")}
-                     
-                     alert("you were dealt two cards: " + currentHand[0].rank + " of " + currentHand[0].suit + " with a value of " + currentHand[0].value + "," + " and " 
-                + currentHand[1].rank + " of " + currentHand[1].suit + " with a value of " + currentHand[1].value); 
+
+                     alert("you have " + currentHand.length + " cards" + " total value is now: " + total); 
 
  
 }
@@ -46,8 +45,8 @@ function Deck() {
 
 function makeDeck() {
 
-    var ranks = new Array("A","2", "3", "4", "5", "6", "7", "8", "9", "10",
-                    "J", "Q", "K");
+    var ranks = new Array("Ace","2", "3", "4", "5", "6", "7", "8", "9", "10",
+                    "Jack", "Queen", "King");
     var suits = new Array("Clubs", "Diamonds", "Hearts", "Spades");
 
     var values = new Array(1,2,3,4,5,6,7,8,9,10,10,10,10)
@@ -61,7 +60,7 @@ function makeDeck() {
             // this was the line i had tried to use for about an hour, it returns the correct 
             // index number of the array but not the string of the name, very very frustrating
 
-             var curCard = {'rank': ranks[j], 'suit':suits[i], 'value':values[j]}
+             var curCard = {'rank': ranks[j], 'suit':suits[i], 'value':values[j], class:(ranks[j]+"-"+ suits[i])}
                      this.deck.push(curCard);
 
            console.log(curCard) 
@@ -89,7 +88,8 @@ var firstPick = function() {
               currentHand.push(fC2);
 
               console.log("there are " + deck.length + " cards left");
-              var valAlert = alert(fC.value+fC2.value);
+              var valAlert = alert("you were dealt two cards: " + fC.rank + " of " + fC.suit + " with a value of " + fC.value + "," + " and " 
+                + fC2.rank + " of " + fC2.suit + " with a value of " + fC2.value + "                               " +  " total value currently is: " +(fC.value+fC2.value));
               
              
               }
@@ -118,7 +118,8 @@ var dealerPick = function() {
               currentHand2.push(dC2);
 
               console.log("there are " + deck.length + " cards left");
-              var valAlert = alert(dC.value+dC2.value);
+              var valAlert = alert("you were dealt two cards: " + dC.rank + " of " + dC.suit + " with a value of " + dC.value + "," + " and " 
+                + dC2.rank + " of " + dC2.suit + " with a value of " + dC2.value + "                         " +  " total value currently is: " +(dC.value+dC2.value));
               
              
               }
@@ -142,20 +143,25 @@ var addDealerCard = function(){
                         var dC3 = deck[Math.floor(Math.random() * deck.length)];
               var rmdC3 = deck.splice(dC3, 1)
 
-              currentHand2.push(dC3); }
-
-              else { console.log}
-}
+              currentHand2.push(dC3); 
+              console.log("dealer was dealt " + dC3.rank + " of " + dC3.suit + " with a value of " + dC3.value + "total value : " + (total+dC3.value))}
+                if (total > 17 && total < 21){
+                console.log( "dealer stands") }
+                if (total > 21){
+                  console.log("Dealer Bussts")
+                }
+              }
 
             // this is not working. i am trying to add a hit function but it is giving me a $)@#U*$ load of $)##@)($!) trouble
  var addPlayerCard = function(){
+  var player;
             var fC3 = deck[Math.floor(Math.random() * deck.length)];
               var rmFC3 = deck.splice(fC3, 1)
 
               currentHand.push(fC3);
               
               console.log("you drew a " + fC3.rank + " of " + fC3.suit + " with a value of " + fC3.value)
-
+              alert("you drew a " + fC3.rank + " of " + fC3.suit + " with a value of " + fC3.value)
 
                var total = 0;
                 for (i=0; i<currentHand.length; i++)
@@ -163,13 +169,17 @@ var addDealerCard = function(){
                     total += currentHand[i].value;
                     }
 
+
                     if (total > 21){ 
-                     console.log("BUSTEDDDDDDDDD")}
+                    player.className = "loser"
+                     console.log("BUSTEDDDDDDDDD")
+                     alert("BUSSSSSSSSTTTTTTEEEEEEEE")
+                     alert("EEEEEEEEDDDDDDDDDDD") }
 
               else {
 
               console.log("you HIT ! NEW HAND VALUE is " + total + " there are " + deck.length + " cards left");
-             
+              alert("you HIT ! NEW HAND VALUE is " + total + " there are " + deck.length + " cards left")
           }
           
   }
@@ -183,7 +193,6 @@ var addDealerCard = function(){
 
            
 
-            
 console.log("===========================================================")
 
     
