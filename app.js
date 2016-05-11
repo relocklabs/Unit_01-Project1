e767f0cf518efaefb704cf9dc344bb0f1198bc33
Andrew Clark <AndrewClark@Andrews-MacBook-Pro.local>
2016-05-11 10:19:50 -0400

    just finished adding dealer functionality

--- app.js
+++ app.js
@@ -3,6 +3,30 @@ var dealer;
 var hand;
   usedCards = []
 
+ var myHand = function(){
+   for (i=0; i<currentHand.length; i++)
+    console.log(currentHand[i].rank + " of " + currentHand[i].suit + " with a value of " + currentHand[i].value)
+    
+    
+  }
+var datValue = function(){
+   var total = 0;
+                for (i=0; i<currentHand.length; i++)
+                {
+                    total += currentHand[i].value;
+                     
+                    }
+
+
+                    if (total > 21){ 
+                     alert("BUSTEDDDDDDDDD")}
+                     
+                     alert("you were dealt two cards: " + currentHand[0].rank + " of " + currentHand[0].suit + " with a value of " + currentHand[0].value + "," + " and " 
+                + currentHand[1].rank + " of " + currentHand[1].suit + " with a value of " + currentHand[1].value); 
+
+ 
+}
+
 function Card (rank, suit, value) {
 
     this.rank = rank;
@@ -63,40 +87,97 @@ var firstPick = function() {
               var rmFC2 = deck.splice(fC2, 1)
 
               currentHand.push(fC2);
-              var valAlert = alert(fC.value+fC2.value);
-             
 
               console.log("there are " + deck.length + " cards left");
-
+              var valAlert = alert(fC.value+fC2.value);
+              
+             
               }
             
 
+              console.log("you were dealt two cards: " + fC.rank + " of " + fC.suit + " with a value of " + fC.value + "," + " and " 
+                + fC2.rank + " of " + fC2.suit + " with a value of " + fC2.value)
+              console.log("current value of hand is " + (fC.value+fC2.value))
+}
+var currentHand2= []
+var dealerPick;
+var dealerPick = function() {
+          currentHand2 = []
+          for (var i = 0; i < 1; i++) {
+            var dC = deck[Math.floor(Math.random() * deck.length)];
+              var rmdC = deck.splice(dC, 1)
+
+              currentHand2.push(dC);
+
+              console.log("there are " + deck.length + " cards left"); 
+             }
+            for (var i = 0; i < 1; i++) {
+            var dC2 = deck[Math.floor(Math.random() * deck.length)];
+              var rmdC2 = deck.splice(dC2, 1)
+
+              currentHand2.push(dC2);
+
+              console.log("there are " + deck.length + " cards left");
+              var valAlert = alert(dC.value+dC2.value);
+              
              
+              }
+            
+
+              console.log("dealer was dealt two cards: " + dC.rank + " of " + dC.suit + " with a value of " + dC.value + "," + " and " 
+                + dC2.rank + " of " + dC2.suit + " with a value of " + dC2.value)
+              console.log("current value of hand is " + (dC.value+dC2.value))
+
+              
 }
 
+var addDealerCard = function(){
+  var total = 0;
+                for (i=0; i<currentHand2.length; i++)
+                {
+                    total += currentHand2[i].value;
+                    }
+
+                    if (total < 17 ){ 
+                        var dC3 = deck[Math.floor(Math.random() * deck.length)];
+              var rmdC3 = deck.splice(dC3, 1)
+
+              currentHand2.push(dC3); }
+
+              else { console.log}
+}
 
             // this is not working. i am trying to add a hit function but it is giving me a $)@#U*$ load of $)##@)($!) trouble
- var addCard = function(){
+ var addPlayerCard = function(){
             var fC3 = deck[Math.floor(Math.random() * deck.length)];
               var rmFC3 = deck.splice(fC3, 1)
 
               currentHand.push(fC3);
               
+              console.log("you drew a " + fC3.rank + " of " + fC3.suit + " with a value of " + fC3.value)
 
-              console.log("you HIT ! there are " + deck.length + " cards left");
 
+               var total = 0;
+                for (i=0; i<currentHand.length; i++)
+                {
+                    total += currentHand[i].value;
+                    }
+
+                    if (total > 21){ 
+                     console.log("BUSTEDDDDDDDDD")}
+
+              else {
+
+              console.log("you HIT ! NEW HAND VALUE is " + total + " there are " + deck.length + " cards left");
+             
           }
           
-  
-
-  var whatsMyHand = function(){
-    console.log(currentHand)
   }
-      
-       
-                     	 
 
-  
+ 
+       
+
+
 
 
 
